77ef53ee993704e87889755b25560542d45978d6
chandzul <dchandzul@gmail.com>
2018-07-09 07:47:51 -0500

    se agrego sass

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
+// Archivos scss del template
+import './scss/app.scss';
+
+// log comando javascrypt
 console.log('Hola mundo');
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
 const config = {
@@ -30,6 +31,26 @@ const config = {
                         }
                     }
                 ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader', 
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
             }
         ]
     },
@@ -37,6 +58,9 @@ const config = {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: 'src/index.pug'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/style.css"
         })
     ]
 };
