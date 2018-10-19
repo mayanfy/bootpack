275254f22c62592bed087b60dbc0b12f3233d73d
David Chandzul <dchandzul@gmail.com>
2018-10-19 02:18:26 -0500

    agrego todas las secciones la 2 y 3 son contenido

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+// Importamos $ para poder usar funciones JQuery
+import $ from 'jquery';
+
+// Archivos Js del framework de bootstrap
+import 'popper.js';
 
 // Archivos Js del framework de bootstrap
 import 'bootstrap';
@@ -23,7 +28,93 @@ AOS.init({
 		this.classList.toggle("is-active");
 	  }, false);
 	});
-  }
+	}
+	
+	$('body').scrollspy({ target: '#main-nav' });
+
+    // Add smooth scrolling
+    $('#main-nav a').on('click', function (e) {
+      // Check for a hash value
+      if (this.hash !== '') {
+        // Prevent default behavior
+        e.preventDefault();
+
+        // Store hash
+        const hash = this.hash;
+
+        // Animate smooth scroll
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 900, function () {
+          // Add hash to URL after scroll
+          window.location.hash = hash;
+        });
+      }
+		});
+		
+	// Init Popover
+	$('[data-toggle="popover"]').popover();
+
+	function showPopover() {
+		$('#hello').popover('show');
+	}
+
+	function hidePopover() {
+		$('#hello').popover('hide');
+	}
+
+	function togglePopover() {
+		$('#hello').popover('toggle');
+	}
+
+	// Popover events
+	$('#hello').on('show.bs.popover', function () {
+		console.log('Popover show');
+	});
+
+	$('#hello').on('shown.bs.popover', function () {
+		console.log('Popover shown');
+	});
+
+	$('#hello').on('hide.bs.popover', function () {
+		console.log('Popover hide');
+	});
+
+	$('#hello').on('hidden.bs.popover', function () {
+		console.log('Popover hidden');
+	});
+
+	// Init tooltips
+	$('[data-toggle="tooltip"]').tooltip();
+
+	function showTooltip() {
+		$('#hello').tooltip('show');
+	}
+
+	function hideTooltip() {
+		$('#hello').tooltip('hide');
+	}
+
+	function toggleTooltip() {
+		$('#hello').tooltip('toggle');
+	}
+
+	// Tooltip Events
+	$('#hello').on('show.bs.tooltip', function () {
+		console.log('Tooltip Show');
+	});
+
+	$('#hello').on('shown.bs.tooltip', function () {
+		console.log('Tooltip Shown');
+	});
+
+	$('#hello').on('hide.bs.tooltip', function () {
+		console.log('Tooltip Hide');
+	});
+
+	$('#hello').on('hidden.bs.tooltip', function () {
+		console.log('Tooltip Hidden');
+	});
 
 // Javascript del template
 console.log('Hola mundo...');
